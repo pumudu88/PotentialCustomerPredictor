21173ee07248e8ce6da588da31b0747516e2eabf
Tharik Keizer <tharik@Thariks-MacBook-Pro.local>
2014-11-17 14:56:59 +0530

    sounded class created

--- modules/cleanser/src/main/java/Cleanser.java
+++ modules/cleanser/src/main/java/Cleanser.java
@@ -2,4 +2,15 @@
  * Created by tharik on 11/17/14.
  */
 public class Cleanser {
+
+    public static void main(String[] args)
+    {
+        try {
+            System.out.println(Soundex.Convert("Tharik"));
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- modules/cleanser/src/main/java/Soundex.java
+++ modules/cleanser/src/main/java/Soundex.java
@@ -1,5 +1,14 @@
+import org.apache.commons.codec.EncoderException;
+
 /**
  * Created by tharik on 11/17/14.
  */
 public class Soundex {
+
+    public static String Convert(String input) throws EncoderException
+    {
+        org.apache.commons.codec.language.Soundex soundex = new org.apache.commons.codec.language.Soundex();
+        return soundex.encode(input);
+
+    }
 }
