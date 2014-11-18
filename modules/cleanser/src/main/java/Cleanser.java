9405347f21363481d84dd9ac49bd596788302687
Tharik Keizer <tharik@Thariks-MacBook-Pro.local>
2014-11-18 09:00:54 +0530

    CSV Reading fixed

--- modules/cleanser/src/main/java/Cleanser.java
+++ modules/cleanser/src/main/java/Cleanser.java
@@ -1,3 +1,7 @@
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+
 /**
  * Created by tharik on 11/17/14.
  */
@@ -6,7 +10,20 @@ public class Cleanser {
     public static void main(String[] args)
     {
         try {
-            System.out.println(Soundex.Convert("Tharik"));
+
+            String csvPath = "/Users/tharik/Desktop/machine learning/Archive/Activity behaviou_Tier2_20141015.csv";
+            String columnName = "Company";
+
+
+            CSVReader reader = new CSVReader(new FileReader(csvPath));
+            String [] nextLine;
+
+            int counter = 0;
+
+            while ((nextLine = reader.readNext()) != null) {
+                System.out.println(counter++  + " - " +Soundex.Convert(nextLine[4]) + " - " +nextLine[4] );
+            }
+
         }
         catch(Exception ex)
         {
