a22d963f120ac880e076c782c8f54c9570a6fda7
Tharik Keizer <tharik@Thariks-MacBook-Pro.local>
2014-11-18 11:31:34 +0530

    reading fixed

--- modules/cleanser/src/main/java/Cleanser.java
+++ modules/cleanser/src/main/java/Cleanser.java
@@ -10,24 +10,37 @@ public class Cleanser {
     public static void main(String[] args)
     {
         try {
-
             String csvPath = "/Users/tharik/Desktop/machine learning/Archive/Activity behaviou_Tier2_20141015.csv";
             String columnName = "Company";
 
+            long startTime = System.currentTimeMillis();
 
             CSVReader reader = new CSVReader(new FileReader(csvPath));
+            String []header = reader.readNext();
+
+
             String [] nextLine;
 
             int counter = 0;
 
             while ((nextLine = reader.readNext()) != null) {
-                System.out.println(counter++  + " - " +Soundex.Convert(nextLine[4]) + " - " +nextLine[4] );
+
+
+                if(nextLine.length > 4 && nextLine[4] != null) {
+                    System.out.println(counter++ + " - " + SoundexMatch.Convert(nextLine[4]) + " - " + nextLine[4]);
+                }
+                else{
+                    System.out.println(counter++ + nextLine[0]);
+                }
             }
+
+            long estimatedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Time taken : "+ estimatedTime/1000 + " seconds");
 
         }
         catch(Exception ex)
         {
-            System.out.println(ex.getMessage());
+            System.out.println("Error Occured " +ex);
         }
     }
 }
