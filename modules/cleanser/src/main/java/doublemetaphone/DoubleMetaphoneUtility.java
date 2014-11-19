b3e6197f27ddd66281570a243e67faf376aa73c9
pumudu <pumudu@wso2.com>
2014-11-19 10:36:50 +0530

    commit metaphone and double metaphone

---
+++ modules/cleanser/src/main/java/doublemetaphone/DoubleMetaphoneUtility.java
@@ -0,0 +1,21 @@
+package doublemetaphone;
+
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.language.DoubleMetaphone;
+
+
+/**
+ * Created by pumudu on 11/18/14.
+ */
+public class DoubleMetaphoneUtility {
+
+
+
+    public static String DoubleMetaphoneConvert(String input) throws EncoderException
+    {
+        DoubleMetaphone doubleMetaphone = new DoubleMetaphone();
+        return doubleMetaphone.encode(input);
+    }
+
+
+}
