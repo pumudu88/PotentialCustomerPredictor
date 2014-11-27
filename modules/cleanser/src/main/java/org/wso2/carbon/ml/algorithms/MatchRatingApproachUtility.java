49fca6d46efd1a8b5ee57fc9ed28a11b6c556e86
pumudu <pumudu@wso2.com>
2014-11-27 10:18:55 +0530

    add new MRA algorithm

---
+++ modules/cleanser/src/main/java/org/wso2/carbon/ml/algorithms/MatchRatingApproachUtility.java
@@ -0,0 +1,22 @@
+package org.wso2.carbon.ml.algorithms;
+
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.language.MatchRatingApproachEncoder;
+
+/**
+ * Created by pumudu on 11/26/14.
+ */
+public class MatchRatingApproachUtility {
+
+    private static MatchRatingApproachEncoder matchRatingApproach = new MatchRatingApproachEncoder();
+
+    public static String Convert(String input) throws EncoderException {
+        return matchRatingApproach.encode(input);
+    }
+
+    public static boolean isStringsEqual(String name1, String name2) {
+
+        return matchRatingApproach.isEncodeEquals(name1,name2);
+    }
+
+}
--- modules/cleanser/src/main/java/org/wso2/carbon/ml/preprocessor/PreProcessor.java
+++ modules/cleanser/src/main/java/org/wso2/carbon/ml/preprocessor/PreProcessor.java
@@ -1,50 +1,160 @@
 package org.wso2.carbon.ml.preprocessor;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.ml.algorithms.MatchRatingApproachUtility;
 import org.wso2.carbon.ml.algorithms.MetaphoneUtility;
 import org.apache.commons.codec.EncoderException;
 import org.wso2.carbon.ml.algorithms.DoubleMetaphoneUtility;
+import org.wso2.carbon.ml.algorithms.SoundexMatchUtility;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pumudu on 11/18/14.
  */
 public class PreProcessor {
 
+    private static final Log logger = LogFactory.getLog(Cleanser.class);
+
     public static void main(String[] args) {
 
 
+
+//        String csvReadFile = "/Users/pumudu/Documents/Machine Lerning/transformed.csv";
+//        String csvWriteCustomerFile = "/Users/pumudu/Documents/Machine Lerning/output/currentCustomers.csv";
+//        String csvWriteNonCustomerFile = "/Users/pumudu/Documents/Machine Lerning/output/currentNonCustomers.csv";
+
+        long startTime = System.currentTimeMillis();
+
+        List<String> nameList = new ArrayList<String>();
+        nameList.add("Hutchcraft");
+        nameList.add("Hadcroft");
+        nameList.add("Hatchard");
+        nameList.add("Hatcher");
+        nameList.add("Hatzar");
+        nameList.add("Hedger");
+        nameList.add("Hitscher");
+        nameList.add("Hodcroft");
+        nameList.add("Hadgraft");
+
+        nameList.add("Braz");
+        nameList.add("Broz");
+
+        nameList.add("ENIS");
+        nameList.add("Emoxa");
+
+        nameList.add("Hilti Befestigungstechnik AG");
+        nameList.add("Hewlett-Packard Co");
+
+        nameList.add("Mathew");
+        nameList.add("Meth");
+
+        nameList.add("Intel Corp");
+        nameList.add("Intel");
+
         try {
-            String metaphoneValueForMicrosoft = MetaphoneUtility.Convert("microsoft");
-            String metaphoneValueForMicrosft  = MetaphoneUtility.Convert("microsof");
+            MetaphoneUtility.setMaxCodeLen(10);
+            DoubleMetaphoneUtility.setMaxCodeLen(4);
 
+            System.out.println("code length metaphone : " + MetaphoneUtility.getMaxCodeLen() );
+            System.out.println("code length double metaphone : " + DoubleMetaphoneUtility.getMaxCodeLen());
 
-            String metaphoneForMicrosoft      = MetaphoneUtility.StringConvert("microsoft");
-            int codeLengthMetaphone           = MetaphoneUtility.getMaxCodeLen();
+            System.out.println("-------------test names-------------");
 
+            for(int i = 0; i < nameList.size(); i++) {
 
-            System.out.println("------ metaphone encode value ------------");
-            System.out.println("metaphone value for microsoft : " + metaphoneValueForMicrosft);
-            System.out.println("metaphone value for microsft : " + metaphoneValueForMicrosoft);
-            System.out.println("code length metaphone : " + codeLengthMetaphone );
+                System.out.println("----"+ nameList.get(i) +"---");
+                System.out.println("soundex          :" + SoundexMatchUtility.Convert(nameList.get(i)));
+                System.out.println("metaphone        :" + MetaphoneUtility.Convert(nameList.get(i)));
+                System.out.println("double metaphone :" + DoubleMetaphoneUtility.Convert(nameList.get(i)));
+                System.out.println("MRA              :" + MatchRatingApproachUtility.Convert(nameList.get(i)));
+            }
 
-            MetaphoneUtility.setMaxCodeLen(5);
-            codeLengthMetaphone           = MetaphoneUtility.getMaxCodeLen();
-            System.out.println("code length metaphone : " + codeLengthMetaphone );
-            metaphoneValueForMicrosft  = MetaphoneUtility.Convert("microsof");
-            System.out.println("metaphone value for microsoft : " + metaphoneValueForMicrosft);
+            long estimatedTime = System.currentTimeMillis() - startTime;
+            logger.info("Time taken : "+ estimatedTime/1000 + " seconds");
 
-
-
-
-            String doubleMetaphoneForMicrosoft = DoubleMetaphoneUtility.Convert("microsoft");
-            String doubleMetaphoneForMicosoft  = DoubleMetaphoneUtility.Convert("micrsoft");
-
-            System.out.println("------ double metaphone encode value ----");
-            System.out.println("double metaphone value for microsoft : " + doubleMetaphoneForMicrosoft);
-            System.out.println("double metaphone value for micosoft  : " + doubleMetaphoneForMicosoft);
 
         } catch (EncoderException e) {
             e.printStackTrace();
+        }  catch (Exception e) {
+            e.printStackTrace();
         }
+
+
+//        try{
+//          System.out.println("--------------------- csv transformed ---------------------------");
+//
+//          long startTime = System.currentTimeMillis();
+//
+//          CSVReader reader=new CSVReader(
+//                    new InputStreamReader(new FileInputStream(csvReadFile), "UTF-8"), ',',CSVReader.DEFAULT_QUOTE_CHARACTER,CSVReader.DEFAULT_QUOTE_CHARACTER);
+//
+//          CSVWriter writerCurrentCustomers = new CSVWriter(new FileWriter(csvWriteCustomerFile), ',', CSVWriter.NO_QUOTE_CHARACTER);
+//          CSVWriter writerNonCustomers     = new CSVWriter(new FileWriter(csvWriteNonCustomerFile), ',', CSVWriter.NO_QUOTE_CHARACTER);
+//
+//            String [] headerLine = reader.readNext();
+//
+//            int isCustomerIndex = 0;
+//            int companyIndex = 0;
+//            int customerIndex = 0;
+//            int indexColumn = 0;
+//
+//            for(int i =0; i < headerLine.length; i++) {
+//                System.out.println("array : " + headerLine[i]);
+//
+//                if(headerLine[i].equals("Is Customer")) {
+//                    isCustomerIndex = i;
+//                }
+//
+//                if(headerLine[i].equals("Company")) {
+//                    companyIndex = i;
+//                }
+//
+//                if(headerLine[i].equals("Customer")) {
+//                    customerIndex = i;
+//                }
+//
+//                if(headerLine[i].equals("Index")) {
+//                    indexColumn = i;
+//                }
+//
+//            }
+//
+//            writerCurrentCustomers.writeNext(headerLine);
+//            writerNonCustomers.writeNext(headerLine);
+//
+//            String [] nextLine;
+//
+//            while ((nextLine = reader.readNext()) != null) {
+//
+//                if(nextLine[isCustomerIndex].equals("true")) {
+//                    writerCurrentCustomers.writeNext(nextLine);
+//                } else {
+//                    writerNonCustomers.writeNext(nextLine);
+//                }
+//
+//
+//            }
+//
+//
+//            long estimatedTime = System.currentTimeMillis() - startTime;
+//            logger.info("Time taken : "+ estimatedTime/1000 + " seconds");
+//
+//          writerNonCustomers.close();
+//          writerCurrentCustomers.close();
+//          reader.close();
+//
+//
+//    } catch (UnsupportedEncodingException e) {
+//        e.printStackTrace();
+//    }  catch (IOException e) {
+//        e.printStackTrace();
+//    }
 
 
     }
