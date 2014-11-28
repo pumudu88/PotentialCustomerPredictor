f01dfdcdbaf53a9503c9d02fb54a5cb4194e4ae2
pumudu <pumudu@wso2.com>
2014-11-29 00:01:41 +0530

    add custom matching algorithm

--- modules/cleanser/src/main/java/org/wso2/carbon/ml/algorithms/CustomMatchingUtility.java
+++ modules/cleanser/src/main/java/org/wso2/carbon/ml/algorithms/CustomMatchingUtility.java
@@ -1,12 +1,103 @@
 package org.wso2.carbon.ml.algorithms;
 
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.commons.codec.EncoderException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pumudu on 11/28/14.
  */
 public class CustomMatchingUtility {
 
+    static List<String> companySuffixList = new ArrayList<String>();
+
+    public static final int BeiderMorseAlgorithm         = 1;
+    public static final int DoubleMetaphoneAlgorithm     = 2;
+    public static final int MatchRatingApproachAlgorithm = 3;
+    public static final int MetaphoneAlgorithm           = 4;
+    public static final int SoundexAlgorithm             = 5;
+
+    public List<String> getCompanySuffixList() {
+        return companySuffixList;
+    }
+
+    public static void setCompanySuffix(String companySuffix) {
+        companySuffixList.add(companySuffix);
+    }
 
 
+    public static String Convert(String input,int algorithmIndex) throws EncoderException {
+
+        String encodedValue = "";
+
+        input = removeCompanyNameSuffix(input);
+
+        switch (algorithmIndex) {
+            case CustomMatchingUtility.BeiderMorseAlgorithm:
+                encodedValue = BeiderMorseUtility.Convert(input);
+            case CustomMatchingUtility.DoubleMetaphoneAlgorithm:
+                encodedValue = DoubleMetaphoneUtility.Convert(input);
+            case CustomMatchingUtility.MatchRatingApproachAlgorithm:
+                encodedValue = MatchRatingApproachUtility.Convert(input);
+            case CustomMatchingUtility.MetaphoneAlgorithm:
+                encodedValue = MetaphoneUtility.Convert(input);
+            case CustomMatchingUtility.SoundexAlgorithm:
+//                encodedValue = SoundexMatchUtility.Convert(input);
+        }
+
+        return encodedValue;
+    }
+
+    private static String removeCompanyNameSuffix(String input) {
+        StringBuffer result = new StringBuffer();
+
+        String[] splitedCompanyName = input.split("\\s+");
+
+        for (int i = 0; i < splitedCompanyName.length; i++) {
+
+            for(int j=0;j<companySuffixList.size();j++) {
+                if(splitedCompanyName[i].equals(companySuffixList.get(j))) {
+                    splitedCompanyName[i] = "";
+                }
+            }
+
+            result.append( splitedCompanyName[i] );
+            result.append(" ");
+        }
+
+        String companyWithOutSuffix = result.toString();
+
+        return companyWithOutSuffix;
+
+    }
+
+    public static void LoadCompanySuffixFromCsv(String csvFilePath) {
+
+        String [] nextLine;
+
+        try {
+
+            CSVReader reader = new CSVReader(
+                new InputStreamReader(new FileInputStream(csvFilePath), "UTF-8"), ',',CSVReader.DEFAULT_QUOTE_CHARACTER,CSVReader.DEFAULT_QUOTE_CHARACTER);
+
+                while ((nextLine = reader.readNext()) != null) {
+                    setCompanySuffix(nextLine[0].toString());
+                }
+
+            reader.close();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
